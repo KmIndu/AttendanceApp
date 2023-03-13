0b831217371f399f898e150d7a17882191415326
Vinay Kumar <champ.vk91@gmail.com>
2023-03-13 18:28:53 +0530

    homescreen

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,11 +10,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double screenHeight = 0;
+  double screenWidth = 0;
+
+  Color primary = const Color.fromARGB(252, 106, 69, 237);
+
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return const Scaffold(
       body: Center(
-        child: Text("HOME"),
+        child: Text(
+          "HOME",
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(2, 2),
+              )
+            ]),
       ),
     );
   }
