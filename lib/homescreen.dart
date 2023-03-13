4c2e52bba0f5a170217990cc846abddac3163451
Vinay Kumar <champ.vk91@gmail.com>
2023-03-13 19:43:40 +0530

    changes

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -33,15 +34,29 @@ class _HomeScreenState extends State<HomeScreen> {
           bottom: 24,
         ),
         decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(40)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10,
-                offset: Offset(2, 2),
-              )
-            ]),
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10,
+              offset: Offset(2, 2),
+            )
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(40)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Expanded(
+                  child: Center(
+                child: Icon(FontAwesomeIcons.calendarDays),
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
